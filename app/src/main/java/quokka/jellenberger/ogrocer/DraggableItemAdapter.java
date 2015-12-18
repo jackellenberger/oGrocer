670cec05bd86c11e6f67d14b626ad3aa07423139
Jack Ellenberger <jellenberger@uchicago.edu>
2015-12-17 18:36:31 -0600

    dinner time

--- app/src/main/java/quokka/jellenberger/ogrocer/DraggableItemAdapter.java
+++ app/src/main/java/quokka/jellenberger/ogrocer/DraggableItemAdapter.java
@@ -1,19 +1,3 @@
-/*
- *    Copyright (C) 2015 Haruki Hasegawa
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package quokka.jellenberger.ogrocer;
 
 import android.support.v4.view.ViewCompat;
@@ -25,22 +9,18 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-//import com.h6ah4i.android.example.advrecyclerview.common.data.AbstractDataProvider;//
-//import com.h6ah4i.android.example.advrecyclerview.common.utils.DrawableUtils;
-//import com.h6ah4i.android.example.advrecyclerview.common.utils.ViewUtils;
 import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter;
 import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemConstants;
 import com.h6ah4i.android.widget.advrecyclerview.draggable.ItemDraggableRange;
 import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractDraggableItemViewHolder;
 
-class MyDraggableItemAdapter
-        extends RecyclerView.Adapter<MyDraggableItemAdapter.MyViewHolder>
-        implements DraggableItemAdapter<MyDraggableItemAdapter.MyViewHolder> {
+class MyDraggableItemAdapter extends RecyclerView.Adapter<MyDraggableItemAdapter.MyViewHolder>
+                             implements DraggableItemAdapter<MyDraggableItemAdapter.MyViewHolder>
+{
     private static final String TAG = "MyDraggableItemAdapter";
 
     // NOTE: Make accessible with short name
-    private interface Draggable extends DraggableItemConstants {
-    }
+    private interface Draggable extends DraggableItemConstants { }
 
     private AbstractDataProvider mProvider;
 
